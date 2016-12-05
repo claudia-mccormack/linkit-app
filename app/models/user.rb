3123d1742bce18ac1bc16d43c0c62d2fe1cba730
Claudia McCormack <mccormack.chc@gmail.com>
2016-12-04 19:54:00 -0500

    added usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :links       
+  has_many :links
+  attr_accessor :login
+      
 end
